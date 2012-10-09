cb62405a22948b9a697ce6ea3f863c9b486fc417
Sam LR <sam.lindenrathen@gmail.com>
2012-10-09 18:39:54 +0100

    minor bug fix in wait_to_quit

--- general_utilities.py
+++ general_utilities.py
@@ -1,12 +1,12 @@
 
 def wait_to_quit():
-    print "press ctrl+C to stop"
+    print "Press ctrl+C to stop"
     try:
         while(True):
             pass
     except KeyboardInterrupt, e:
-        pass
-    print "bye bye"
+        print "bye bye"
+        return
 
   
 
