fa67f10c68477d2e993a70bf3c94d399a17d5167
Sam LR <sam.lindenrathen@gmail.com>
2012-10-11 13:08:50 +0100

    added draw func & funcs to find value X from a set of hists

--- root_utilities.py
+++ root_utilities.py
@@ -8,7 +8,11 @@ Created by Sam Cook on 2012-07-25.
 Generally useful functions for dealing with ROOT objects
 """
 
-from ROOT import gROOT, TFile, TTree, TBranch, TCanvas, TH1F
+from ROOT import gROOT, TFile, TTree, TBranch, TCanvas, TH1F, TLegend
+
+from general_utilities import get_quantised_width_height
+
+from sys import maxint
 
 class ROOTException(Exception):
     pass
@@ -129,6 +133,101 @@ def set_bin_val_er_label(hist, bin, val, er, bin_name):
     hist.SetBinContent(bin, float(val))
     hist.SetBinError(bin, float(er))
     hist.GetXaxis().SetBinLabel(bin, str(bin_name))
+
+
+def __get_x_amongst_hists(hists, initial_val, x_func, comp_func):
+    """
+    Helper function; apply x_func to all histograms in hists
+    if comp_func(previous_val, returned_val) is true
+    then returned_val set.
+    """
+    if hasattr(hists, 'keys'): hists = hists.values()
+    res = initial_val
+    for hist in hists:
+        val = x_func(hist)
+        if comp_func(res, val):
+            res = val
+    return res
+
+
+def get_max_amongst_hists(hists):
+    """
+    Find the maximum value amongst hists
+    """
+    return __get_x_amongst_hists(hists, (-maxint - 1), 
+                                lambda hist: hist.GetMaximum(),
+                                lambda prev, this_val: prev < this_val)
+        
+
+
+def get_min_amongst_hists(hists):
+    """
+    Find the minimum value amongst hists
+    """
+    return __get_x_amongst_hists(hists, maxint,
+                                lambda hist: hist.GetMinimum(),
+                                lambda prev, this_val: prev > this_val)
+
+
+def get_canvas_with_hists(hists, draw_opt='', canvas_name='c', set_y_min=False,
+                        pad_preffix='', legend_preffix=''):
+    """
+    Draws given hists to a canvas. Assumes hists is either a dictionary
+    of 
+        <hist title>:<histogram> 
+    pairs or a dictionary of 
+        <hist title>:{<sub histogram title>:<sub histograms>} 
+    pairs. 
+    For either a single canvas will be divided into pads (1 per pair), 
+    in the later case the sub-histograms will be all drawn to the same pad
+    and the sub histogram title will be used in the appropriate legend.
+    
+    draw_opt are any ROOT drawing options to be used.
+    canvas_name is a prefix to use for the canvas name (a unique ID will 
+    be added)
+    set_y_min toggles setting the minimum y value, useful for small ranges
+    of values far from the origin.
+    """
+    # use a function attribute to make all canvases unique
+    if not hasattr(get_canvas_with_hists, "canvas_id"):
+        get_canvas_with_hists.canvas_id = 0
+    else:
+        get_canvas_with_hists.canvas_id += 1
+    canvas_name = canvas_name + str(get_canvas_with_hists.canvas_id)
+    # figure out how many pads the canvas needs to be divided into
+    x,y = get_quantised_width_height(len(hists))
+    canvas = make_canvas(canvas_name, n_x=x, n_y=y, maximised=True)
+    canvas.save_legends = {}
+    for pad_id, (pad_name, obj_to_draw) in enumerate(hists.items(), 1):
+        pad = canvas.cd(pad_id)
+        if not hasattr(obj_to_draw, 'keys'):
+            # single hist draw it and move on
+            obj_to_draw.SetTitle(pad_name)
+            obj_to_draw.Draw(draw_opt)
+        else:
+            # collection of hists, to be drawn on the same pad
+            axis_max = get_max_amongst_hists(obj_to_draw)
+            if set_y_min: axis_min = get_min_amongst_hists(obj_to_draw)
+            legend = TLegend(0.6, 0.9-0.04*len(obj_to_draw),0.8,0.9)
+            legend.SetFillColor(0)
+            first_hist = None
+            for colour_id, (hist_name, hist) in enumerate(obj_to_draw.items(),1):
+                if first_hist==None:
+                    hist.SetMaximum(1.1*axis_max)
+                    if set_y_min: hist.SetMinimum(0.9*axis_min)
+                    hist.Draw(draw_opt)
+                    first_hist=hist
+                else:
+                    hist.SetLineColor(colour_id)
+                    hist.Draw(draw_opt+"SAME")
+                legend.AddEntry(hist, legend_preffix + hist_name)
+            first_hist.SetTitle(pad_preffix+str(pad_name)) 
+            legend.Draw()
+            # make sure that the legend objects stay in scope
+            # mmmmm smells like impending memory leak!
+            canvas.save_legends[pad_id] = legend
+            pad.Update()
+    return canvas
 
 
 if __name__ == '__main__':
