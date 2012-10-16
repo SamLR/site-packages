8242150a40521da3b4a627dc6481505518c69071
Sam LR <sam.lindenrathen@gmail.com>
2012-10-16 20:52:13 +0100

    make_hist now works for any dimension of hist, also uses TH1D

--- root_utilities.py
+++ root_utilities.py
@@ -8,7 +8,8 @@ Created by Sam Cook on 2012-07-25.
 Generally useful functions for dealing with ROOT objects
 """
 
-from ROOT import gROOT, TFile, TTree, TBranch, TCanvas, TH1F, TLegend
+from ROOT import gROOT, TFile, TTree, TBranch, TCanvas, TLegend, \
+                 TH1D, TH2D, TH3D
 
 from general_utilities import get_quantised_width_height
 
@@ -45,12 +46,83 @@ def get_struct(struct_fmt, struct_name):
     
 
 
-def make_hist(name, xmin, xmax, xtitle, ytitle, description=None):
-    description = description if description != None else name
-    res = TH1F(name, description, (xmax - xmin), xmin, xmax)
-    res.GetXaxis().SetTitle(xtitle)
-    res.GetYaxis().SetTitle(ytitle)
+def make_hist(name, mins=0, maxs=100, titles=None, bins=None, dim=1, des=None):
+    """
+    Make a named ROOT histogram. 
+    
+    Mins, maxs and bins can be supplied either as a single number to be 
+    used for all axis, an indexable object (indexes: 0,1,2). If not supplied
+    the defaults of 0, 100 and min-max (of that axis) will be used.
+    
+    If not supplied the number of bins per axis will be the difference 
+    between the minimum and maximum (or 10 if the difference is less than 1)
+    
+    Titles will be set if supplied and are assumed be in the order (x,y,z)
+    """
+    dim = int(dim)
+    if dim not in (1,2,3):
+        raise ROOTException("dim must between 1 & 3")
+    
+    description = des if des else name
+    
+    # make the argument list
+    args = [name, description,]
+    for d in range(dim):
+        t_min = mins if not hasattr(mins, '__len__') else mins[d]
+        t_max = maxs if not hasattr(maxs, '__len__') else maxs[d]
+        if hasattr(bins, '__len__'):
+            t_bin = bins[d]
+        elif bins:
+            t_bin = bins
+        else:
+            t_bin = int(t_max - t_min) if int(t_max - t_min) > 1 else 1
+        if (t_min>t_max): raise ROOTException("min > max!")
+        args += [t_bin, t_min, t_max]
+    
+    if dim == 1:
+        res = TH1D(*args)
+    elif dim == 2:
+        res = TH2D(*args)
+    elif dim == 3:
+        res = TH3D(*args)
+        
+        
+    if titles and len(titles) >= 1: res.GetXaxis().SetTitle(titles[0])
+    if titles and len(titles) >= 2: res.GetYaxis().SetTitle(titles[1])
+    if titles and len(titles) >= 3: res.GetZaxis().SetTitle(titles[2])
+    
     return res
+
+
+def test_make_hist():
+    from time import sleep
+    t = []
+    sleep_time = 1.5
+    mins = (-50, -150, -250)
+    maxs = (50, 150, 250)
+    bins = (10, 5, 20)
+    fills = ((1,), (1,2), (1,2,3))
+    titles = ("x", "y", "z")
+    draw_opt = ("", "CONTZ", "BOX")
+    can = make_canvas("n", 2, 2)
+    for i in range(3):
+        can.cd(i+1)
+        t.append( make_hist("h2_"+str(i), mins, maxs, titles,bins, dim=i+1))
+        t[i].Fill(*fills[i])
+        t[i].Draw(draw_opt[i])
+    
+    try:
+        make_hist("h_fail", dim = 42)
+    except ROOTException, e:
+        print e, "Hooray if you see this!"
+    
+    try:
+        make_hist("h_fail2", mins=42, maxs=2)
+    except ROOTException, e:
+        print e, "Hooray if you see this!"
+    
+    print "All tests passed (assuming visual inspection of histograms)"
+    sleep (10)
 
 
 def rebin_nbins(hist, n_bins, new_name=''):
@@ -231,5 +303,5 @@ def get_canvas_with_hists(hists, draw_opt='', canvas_name='c', set_y_min=False,
 
 
 if __name__ == '__main__':
-    pass
+    test_make_hist()
 
